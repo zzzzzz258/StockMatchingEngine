432dde55259eeb62ee49d41b013f73fdf1566e16
zz258@duke.edu <zz258@vcm-25372.vm.duke.edu>
2022-03-29 12:12:41 -0400

    remove useless files

---
+++ server/src/test/java/edu/duke/ece568/em/server/PositionTest.java
@@ -0,0 +1,13 @@
+package edu.duke.ece568.em.server;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PositionTest {
+  @Test
+  public void test_() {
+
+  }
+
+}
