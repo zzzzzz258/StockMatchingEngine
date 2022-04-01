8455aed42aee379c49c239bf82eafd7439d37324
afsana.chowdhury <afsana.chowdhury@duke.edu>
2022-04-01 11:06:22 -0400

    Added latency calculation

--- client/src/main/java/edu/duke/ece568/em/client/MultiClient.java
+++ client/src/main/java/edu/duke/ece568/em/client/MultiClient.java
@@ -1,16 +1,18 @@
 package edu.duke.ece568.em.client;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class MultiClient {
-  final int numOfThreads = 100;
+  final int numOfThreads = 1000;
   final CyclicBarrier barrier = new CyclicBarrier(numOfThreads, new scalabilityCalculator());
+  private ArrayList<Long> reqLatencies = new ArrayList<Long>();
 
   class scalabilityCalculator implements Runnable {
     private long startTime;
-
     public scalabilityCalculator() {
       this.startTime = Instant.now().getEpochSecond();
       System.out.println(
@@ -23,9 +25,18 @@ public class MultiClient {
       System.out.println(
           "*******************************" + "Received all responses..." + "*******************************\n");
 
+      // Calculate throughput
       long diff = endTime - startTime;
       long thruPut = numOfThreads/diff;
       System.out.println("Possible throughput of the server: " + thruPut + " requests/second");
+
+      // Calculate Avg Latency
+      long sum = 0;
+      for (long latency : reqLatencies) {
+        sum += latency;
+      }
+      float avg = (sum/numOfThreads)/1000;
+      System.out.println("Average latency of each request: " + avg + " seconds");
     }
   }
 
@@ -41,7 +52,11 @@ public class MultiClient {
       int portNum = 12345;
       try {
         Client theClient = new Client(hostname, portNum);
+        long reqSentAt = System.currentTimeMillis();
         theClient.runSampleCreateTest(String.valueOf(reqID), "sym");
+        long reqCompleteAT = System.currentTimeMillis();
+        long reqLatency = reqCompleteAT - reqSentAt;
+        reqLatencies.add(reqLatency);
       } catch (Exception e) {
         System.out.println("Error in connecting to server: " + e.getMessage());
       }
